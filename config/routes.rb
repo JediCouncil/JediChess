c7194097e63c50cfa9bfbd7b1f0e30e657f4f69f
Jenny Mangaly <jmangaly@gmail.com>
2015-12-18 04:08:04 +0000

    St up CRUD for Games model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
+    resources :games, :only =>[:new, :create, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
