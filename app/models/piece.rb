45f052e8ca7182cd91daef95ead5fa99c92f8c9c
eric orellana <eric.orellana92@gmail.com>
2015-12-23 02:59:42 +0000

    minor revisions for readability

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -32,19 +32,19 @@ class Piece < ActiveRecord::Base
     end
   end
 
-  def set_x_coordinates(starting_x, destination_x)
-    if destination_x > x
-      x_coordinates = (x...destination_x).to_a[1..-1]
+  def set_x_coordinates(destination_x)
+    if destination_x > self.x
+      x_coordinates = (self.x...destination_x).to_a[1..-1]
     else
-      x_coordinates = (destination_x...x).to_a.reverse[0..-2]
+      x_coordinates = (destination_x...self.x).to_a.reverse[0..-2]
     end
   end
 
-  def set_y_coordinates(starting_y, destination_y)
-    if destination_y > y
-      y_coordinates = (y...destination_y).to_a[1..-1]
+  def set_y_coordinates(destination_y)
+    if destination_y > self.y
+      y_coordinates = (self.y...destination_y).to_a[1..-1]
     else
-      y_coordinates = (destination_y...y).to_a.reverse[0..-2]
+      y_coordinates = (destination_y...self.y).to_a.reverse[0..-2]
     end
   end
 
@@ -61,21 +61,21 @@ class Piece < ActiveRecord::Base
   end
 
   def diagonal_obstruction?(destination_x, destination_y)
-    x_coordinates = set_x_coordinates(x, destination_x)
-    y_coordinates = set_y_coordinates(y, destination_y)
+    x_coordinates = set_x_coordinates(destination_x)
+    y_coordinates = set_y_coordinates(destination_y)
 
     check_coordinates(x_coordinates, y_coordinates)
   end
 
   def vertical_obstruction?(destination_x, destination_y)
-    y_coordinates = set_y_coordinates(y, destination_y)
+    y_coordinates = set_y_coordinates(destination_y)
     x_coordinates = [self.x] * y_coordinates.count
 
     check_coordinates(x_coordinates, y_coordinates)
   end
 
   def horizontal_obstruction?(destination_x, destination_y)
-    x_coordinates = set_x_coordinates(x, destination_x)
+    x_coordinates = set_x_coordinates(destination_x)
     y_coordinates = [self.y] * x_coordinates.count
 
     check_coordinates(x_coordinates, y_coordinates)
