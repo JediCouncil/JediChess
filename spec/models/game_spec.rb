402f227ebf1ae503662983d5f64170c781418d31
Yan Hu <iamyanhu@gmail.com>
2015-12-27 22:43:38 +0000

    fixing a model query statement syntax in rspec game_spec.rb

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Game, type: :model do
   it "populate board" do
   	game = FactoryGirl.create(:game)
 
-  	expect(game.pieces.where(:type => 'Rook', :color =>'white', :y =>1).select('x')).to contain_exactly('a', 'h')
+  	expect(game.pieces.where(:x => 'a', :y =>1).select('type')).to eq('Rook')
   	# expect(game.pieces.where(:type => 'Rook', :color =>'white').y).to eq(2)
   end 
 end
