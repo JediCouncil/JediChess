25bde750eb5fe69356883c7345ff238f813a86f6
Yan Hu <iamyanhu@gmail.com>
2016-01-02 18:17:29 +0000

    rendered pieces using sprite and js in the show file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  helper GamesHelper
 
   def index
     @games = Game.available
@@ -15,6 +16,17 @@ class GamesController < ApplicationController
 
 	def show
 		@game = Game.find(params[:id])
+ 
+    @pos_id_array = []
+    @piece_class_array = [] #will be accessed by the template
+
+    @game.pieces.each do |piece|
+      pos_id = piece.x + piece.y.to_s #gon.pos_id = a8
+      piece_class = piece.color + '_' + piece.type.downcase # gon.piece_class = white_knight
+      @pos_id_array << pos_id
+      @piece_class_array << piece_class
+    end
+
 	end
 
 end
