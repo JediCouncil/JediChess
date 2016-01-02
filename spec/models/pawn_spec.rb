f9331b4cef7cef850a4763e546e2fe9872c21758
eric orellana <eric.orellana92@gmail.com>
2016-01-02 22:16:33 +0000

    rspec tests created for valid_move? method for pawn

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid_move?" do
+    let(:pawn) { build(:pawn) }
+
+    context "first move" do
+      it "can move 2 spaces" do
+      end
+
+      it "can move 1 space" do
+      end
+
+      it "can't move 3 spaces" do
+      end
+    end
+    context "not first move" do
+      it "can move 1 space" do
+      end
+
+      it "can't move 2 spaces" do
+      end
+
+      it "can't move backwards" do
+      end
+    end
+    context "capture" do
+      it "can capture one square diagonally" do
+      end
+
+      it "can't capture vertically" do
+      end
+
+      it "can't capture horizontally" do
+      end
+    end
+  end
 end
