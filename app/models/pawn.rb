01a51f0b7b74dbe36499c93841b853925f4ef1b0
eric orellana <eric.orellana92@gmail.com>
2016-01-03 04:11:35 +0000

    added rspec tests and valid_move? method is wip

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,47 @@
 class Pawn < Piece
 
   def valid_move?(destination_x, destination_y)
+    #invalid move error raises if moving to same current
+    #xy coordinates
+
+    #check if path is obstructed
+    obstructed = is_obstructed?(destination_x, destination_y)
+    return false if obstructed
+
+    #check if it's backward move
+    backwards = backward_move?(destination_y)
+    return false if backwards
+
+    #find distance
+    y_coords = current_to_destination_y_coordinates(destination_y)
+    y_distance = y_coords.length
+
+    if first_move?
+      y_distance <= 1 ? true : false
+    else
+      #move only 1 space
+      if y_distance < 1
+        true
+      else
+        false
+      end
+    end
   end
 
+  def first_move?
+    if black?
+      y == 2 ? true : false#starting y-cord is 2
+    else
+      y == 7 ? true : false#starting y-cord is 7
+    end
+  end
+
+  def backward_move?(destination_y)
+    if black?
+      destination_y < y ? true : false
+    else
+      destination_y > y ? true : false
+    end
+  end
 end
+
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -2,34 +2,83 @@ require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
   describe "#valid_move?" do
-    let(:white_pawn) { build(:pawn, color: "white") }
-
     context "first move" do
       let(:black_pawn) { build(:pawn, color: "black", x: 'B', y: 2) }
+      let(:white_pawn) { build(:pawn, color: "white", x: 'C', y: 7) }
 
-      it "can move 2 spaces" do
+      it "black pawn can move 2 spaces" do
         result = black_pawn.valid_move?('B', 4)
         expect(result).to be true
       end
 
-      it "can move 1 space" do
+      it "black pawn can move 1 space" do
         result = black_pawn.valid_move?('B', 3)
         expect(result).to be true
       end
 
-      it "can't move 3 spaces" do
+      it "black pawn can't move 3 spaces" do
         result = black_pawn.valid_move?('B', 5)
+        expect(result).to be false
+      end
+
+       it "black pawn can't move backwards" do
+        result = black_pawn.valid_move?('B', 1)
+        expect(result).to be false
+      end
+
+      it "white pawn can move 2 spaces" do
+        result = white_pawn.valid_move?('C', 5)
+        expect(result).to be true
+      end
+
+      it "white pawn can move 1 space" do
+        result = white_pawn.valid_move?('C', 6)
+        expect(result).to be true
+      end
+
+      it "white pawn can't move 3 spaces" do
+        result = white_pawn.valid_move?('C', 4)
+        expect(result).to be false
+      end
+
+      it "white pawn can't move backwards" do
+        result = white_pawn.valid_move?('C', 8)
         expect(result).to be false
       end
     end
     context "not first move" do
-      it "can move 1 space" do
+      let(:black_pawn) { build(:pawn, color: "black", x: 'C', y: 4) }
+      let(:white_pawn) { build(:pawn, color: "white", x: 'B', y: 6) }
+      let(:obstruent_piece) {build(:piece)}
+
+      it "black_pawn can move 1 space" do
+        result = black_pawn.valid_move?('C', 5)
+        expect(result).to be true
       end
 
-      it "can't move 2 spaces" do
+      it "black_pawn can't move 2 spaces" do
+        result = black_pawn.valid_move?('C', 6)
+        expect(result).to be false
+      end
+
+      it "black_pawn can't move backwards" do
+        result = black_pawn.valid_move?('C', 2)
+        expect(result).to be false
+      end
+
+      it "white_pawn can move 1 space" do
+        result = white_pawn.valid_move?('B', 5)
+        expect(result).to be true
+      end
+
+      it "white_pawn can't move 2 spaces" do
+        result = white_pawn.valid_move?('B', 4)
+        expect(result).to be false
       end
 
       it "can't move backwards" do
+        result = white_pawn.valid_move?('B', 7)
+        expect(result).to be false
       end
     end
     context "capture" do
