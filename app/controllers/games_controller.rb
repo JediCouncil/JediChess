9e3a469176247cf9fa17ab4fffcc5ee2a09e0473
Yan Hu <iamyanhu@gmail.com>
2016-01-04 05:07:29 +0000

    refactored render-pieces using a helper function

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  helper GamesHelper
+  include GamesHelper 
 
   def index
     @games = Game.available
@@ -16,17 +16,8 @@ class GamesController < ApplicationController
 
 	def show
 		@game = Game.find(params[:id])
- 
-    @pos_id_array = []
-    @piece_class_array = [] #will be accessed by the template
-
-    @game.pieces.each do |piece|
-      pos_id = piece.x + piece.y.to_s #gon.pos_id = a8
-      piece_class = piece.color + '_' + piece.type.downcase # gon.piece_class = white_knight
-      @pos_id_array << pos_id
-      @piece_class_array << piece_class
-    end
-
+    @pieces_hash = render_pieces(@game) #call the helper function and get the hash. will be passed onto the view
+    
 	end
 
 end
