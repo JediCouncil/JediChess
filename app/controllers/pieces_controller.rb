9389e4fbbc80d0ebf58f99c0877b12528a0d2ca2
eric orellana <eric.orellana92@gmail.com>
2016-01-07 18:10:26 +0000

    rspec tests wip for pieces update action

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,16 +1,15 @@
 class PiecesController < ApplicationController
   include GamesHelper
+
   def show
-    # binding.pry
     @piece = current_piece
     @game = @piece.game
     @pieces_hash = render_pieces(@game)
-
-    @piece_class = @piece.color + '_' + @piece.type.downcase
     @piece_pos_id = @piece.x + @piece.y.to_s
   end
 
   def update
+    # binding.pry
     current_piece.update_attributes(piece_params)
     @game = current_piece.game
     redirect_to game_path(@game)
@@ -19,11 +18,12 @@ class PiecesController < ApplicationController
   private
 
   helper_method :curent_piece
+
   def current_piece
     @current_piece ||= Piece.find(params[:id])
   end
 
   def piece_params
     params.require(:piece).permit(:x, :y)
-   end
+  end
 end
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
+  let(:piece){ create(:piece, x: 'B', y: 2) }
+
+  describe "PUT #update" do
+    it "updates piece's xy coordinates" do
+      put :update, id: piece, piece: {x: 'B', y: 3}
+      piece.x.should eq('B')
+      piece.y.should eq(3)
+    end
+  end
 end
