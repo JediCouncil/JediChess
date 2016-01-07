930489080345968768b1acbbe610db4055380398
Rebecca Taylor <inflamedmuse@gmail.com>
2016-01-07 04:09:35 +0000

    Took about is_obstructed as wasn't needed for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,23 +1,15 @@
 class King < Piece
 	def valid_move?(destination_x, destination_y)
-		# not a valid move if obstructed
-		return false if is_obstructed?(destination_x, destination_y)
-
-		# get x_coord_distance
 		x_coord_distance = 0
 		x_coord_indices = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8 }
 		x_coord_distance += x_coord_indices[destination_x] - x_coord_indices[x]
 	
-		# get y_coord_distance
 		y_coord_distance = 0
 		y_coord_distance += destination_y - y
 		
 		if x_coord_distance <= 1 && y_coord_distance <= 1
-			# valid move
 			return true
 		else
-			# this is for if the king moves 
-			# more than 1 step; will not be valid move
 			return false
 		end
 	end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,35 +1,29 @@
+require 'rails_helper'
+
 RSpec.describe King, type: :model do
 
   describe '#valid_move?' do
     let(:king)  { build(:king, x: 'D' , y: 4) }
-    let(:piece) { build(:piece, x: 'D', y: 3) }
 
-    context 'the king is_obstructed' do
-      it 'will not move' do
-        result = king.valid_move?('D', 3)
-        expect(result).to be false
-      end
-    end
-
-    context 'the king tries to move more than one step' do
-      it 'will not move' do
+    context 'given king tries to move more than one step' do
+      it 'is not a valid move' do
         result = king.valid_move?('D', 7)
         expect(result).to be false
       end
     end
 
-    context 'the king has a successful move' do
-      it 'moves the king vertically' do
+    context 'given king has a successful move path' do
+      it 'can move the king vertically' do
         result = king.valid_move?('D', 5)
         expect(result).to be true
       end
 
-      it 'moves the king diagonally' do
+      it 'can move the king diagonally' do
         result = king.valid_move?('C', 3)
         expect(result).to be true
       end
 
-      it 'moves the king horizontally' do
+      it 'can move the king horizontally' do
         result = king.valid_move?('C', 4)
         expect(result).to be true
       end
