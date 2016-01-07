3a9081d530c4c42657e3ac18cca6ea2f1bc3a880
eric orellana <eric.orellana92@gmail.com>
2016-01-07 19:15:09 +0000

    ran rubocop

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-  let(:piece){ create(:piece, x: 'B', y: 2) }
+  let(:piece) { create(:piece, x: 'B', y: 2) }
 
-  describe "PUT #update" do
+  describe 'PUT #update' do
     it "updates piece's xy coordinates" do
-      put :update, id: piece, piece: {x: 'D', y: 4}
+      put :update, id: piece, piece: { x: 'D', y: 4 }
       piece.reload
       expect(piece.x).to eq('D')
       expect(piece.y).to eq(4)
     end
-    it "redirects to the game show page" do
-      put :update, id: piece, piece: {x: 'B', y: 2}
+    it 'redirects to the game show page' do
+      put :update, id: piece, piece: { x: 'B', y: 2 }
       expect(response).to redirect_to(game_path(piece.game))
     end
   end
