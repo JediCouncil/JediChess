90f993026381e908a5b0219bebca3e133429c615
eric orellana <eric.orellana92@gmail.com>
2016-01-08 20:10:23 +0000

    rspec tests for update action for pieces controller and rspec tests for move_to! method for pieces revised, and removed game association in pieces factory

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-  let(:piece) { create(:piece, x: 'B', y: 2) }
 
   describe 'PUT #update' do
+    let(:game) { create(:game) }
+    let(:piece) { game.pieces.first }
+
     it "updates piece's xy coordinates" do
-      put :update, id: piece, piece: { x: 'D', y: 4 }
+      put :update, id: piece, piece: { x: 'B', y: 1 }
       piece.reload
-      expect(piece.x).to eq('D')
-      expect(piece.y).to eq(4)
+      expect(piece.x).to eq('B')
+      expect(piece.y).to eq(1)
     end
     it 'redirects to the game show page' do
-      put :update, id: piece, piece: { x: 'B', y: 2 }
-      expect(response).to redirect_to(game_path(piece.game))
+      put :update, id: piece, piece: { x: 'B', y: 1 }
+      expect(response).to redirect_to(game_path(game))
     end
   end
 end
--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :piece do
-    association :game
     factory :pawn, class: 'Pawn' do
     end
   end
