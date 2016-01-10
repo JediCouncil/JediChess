e2f9852890cfff0982f4ca3c7e0030370eaa4046
eric orellana <eric.orellana92@gmail.com>
2016-01-10 01:17:09 +0000

    rubocop autocorrect and fixed grammer error on rspec test

--- app/models/pieces/king.rb
+++ app/models/pieces/king.rb
@@ -1,12 +1,12 @@
 class King < Piece
   def can_castle?(rook)
-    return false unless first_move == "true"
+    return false unless first_move == 'true'
 
-    return false unless rook.first_move == "true"
+    return false unless rook.first_move == 'true'
 
     return false if is_obstructed?(rook.x, rook.y)
 
-    return true
+    true
   end
 
   def castle!(rook)
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,63 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-  describe "#can_castle" do
+  describe '#can_castle' do
     let(:black_king) { build(:king, x: 'E', y: 8, color: 'black') }
     let(:black_rook) { build(:rook, x: 'A', y: 8, color: 'black') }
     let(:white_king) { build(:king, x: 'E', y: 1, color: 'white') }
     let(:white_rook) { build(:rook, x: 'A', y: 1, color: 'white') }
 
-    context "king and rook unmoved and no obstructions" do
-      it "returns true" do
+    context 'king and rook unmoved and no obstructions' do
+      it 'returns true' do
         result = black_king.can_castle?(black_rook)
         expect(result).to be true
       end
-       it "returns true" do
+      it 'returns true' do
         result = white_king.can_castle?(white_rook)
         expect(result).to be true
       end
     end
-    context "king moved, and no obstructions" do
-      it "returns false" do
-        black_king.first_move = "false"
+    context 'king moved, and no obstructions' do
+      it 'returns false' do
+        black_king.first_move = 'false'
         result = black_king.can_castle?(black_rook)
         expect(result).to be false
       end
-       it "returns false" do
-        white_king.first_move = "false"
+      it 'returns false' do
+        white_king.first_move = 'false'
         result = white_king.can_castle?(white_rook)
         expect(result).to be false
       end
     end
-    context "rook moved, and no obstructions" do
-      it "returns false" do
-        black_rook.first_move = "false"
+    context 'rook moved, and no obstructions' do
+      it 'returns false' do
+        black_rook.first_move = 'false'
         result = black_king.can_castle?(black_rook)
         expect(result).to be false
       end
-      it "returns false" do
-        white_rook.first_move = "false"
+      it 'returns false' do
+        white_rook.first_move = 'false'
         result = white_king.can_castle?(white_rook)
         expect(result).to be false
       end
     end
   end
 
-  describe "#castle!" do
+  describe '#castle!' do
     let(:black_king) { build(:king, x: 'E', y: 8, color: 'black') }
     let(:black_rook) { build(:rook, x: 'A', y: 8, color: 'black') }
     let(:white_king) { build(:king, x: 'E', y: 1, color: 'white') }
     let(:white_rook) { build(:rook, x: 'A', y: 1, color: 'white') }
 
-    context "queenside" do
-      it "black king moves 2 spaces to the left and black rook moves to the right of the king" do
+    context 'queenside' do
+      it 'black king moves 2 spaces to the left and black rook moves to the right of the king' do
         black_king.castle!(black_rook)
         expect(black_king.x).to eq('C')
         expect(black_king.y).to eq(8)
         expect(black_rook.x).to eq('D')
         expect(black_rook.y).to eq(8)
       end
-      it "white king moves 2 spaces to the left and white rook moves to the right of the king" do
+      it 'white king moves 2 spaces to the left and white rook moves to the right of the king' do
         white_king.castle!(white_rook)
         expect(white_king.x).to eq('C')
         expect(white_king.y).to eq(1)
@@ -65,8 +65,8 @@ RSpec.describe King, type: :model do
         expect(white_rook.y).to eq(1)
       end
     end
-    context "kingside" do
-      it "black king moves 2 spaces to the right and black rook moves to the left of the king and" do
+    context 'kingside' do
+      it 'black king moves 2 spaces to the right and black rook moves to the left of the king' do
         black_rook.update(x: 'H', y: '8')
         black_king.castle!(black_rook)
         expect(black_king.x).to eq('G')
@@ -74,7 +74,7 @@ RSpec.describe King, type: :model do
         expect(black_rook.x).to eq('F')
         expect(black_king.y).to eq(8)
       end
-       it "white king moves 2 spaces to the left and white rook moves to the right of the king" do
+      it 'white king moves 2 spaces to the left and white rook moves to the right of the king' do
         white_rook.update(x: 'H', y: '1')
         white_king.castle!(white_rook)
         expect(white_king.x).to eq('G')
@@ -85,4 +85,3 @@ RSpec.describe King, type: :model do
     end
   end
 end
-
