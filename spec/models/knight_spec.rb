6e890be063ae4b203cb02bee59c6a4b461abe4c6
Rebecca Taylor <inflamedmuse@gmail.com>
2016-01-10 02:55:16 +0000

    more knight tests added and passed

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -24,17 +24,57 @@ RSpec.describe Knight, type: :model do
         result = knight.valid_move?('E', 5)
         expect(result).to be true
       end
+
+      it 'is a valid move' do
+        result = knight.valid_move?('B', 6)
+        expect(result).to be true
+      end
+
+      it 'is a valid move' do
+        result = knight.valid_move?('A', 5)
+        expect(result).to be true
+      end
+
+      it 'is a valid move' do
+        result = knight.valid_move?('D', 2)
+        expect(result).to be true
+      end
+
+      it 'is a valid move' do
+        result = knight.valid_move?('E', 3)
+        expect(result).to be true
+      end
     end
 
     context 'given knight doesn\'t move properly' do
       it 'is not a valid move' do
         result = knight.valid_move?('H', 3)
-        expect(result).to be true
+        expect(result).to be false
       end
 
       it 'is not a valid move' do
         result = knight.valid_move?('C', 8)
-        expect(result).to be true
+        expect(result).to be false
+      end
+
+      it 'is not a valid move' do
+        result = knight.valid_move?('A', 1)
+        expect(result).to be false
+      end
+
+      it 'is not a valid move' do
+        result = knight.valid_move?('C', 3)
+        expect(result).to be false
+      end
+
+      it 'is not a valid move' do
+        result = knight.valid_move?('E', 4)
+        expect(result).to be false
+      end
+
+      it 'is not a valid move' do
+        result = knight.valid_move?('B', 5)
+        expect(result).to be false
       end
     end
   end
