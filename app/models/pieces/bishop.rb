bfe37ef09cce3ac8ef2da6069b6d2a507341f334
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2016-01-16 11:10:26 -0800

    pseudo code with jen

--- app/models/pieces/bishop.rb
+++ app/models/pieces/bishop.rb
@@ -1,29 +1,28 @@
 class Bishop < Piece
 
   #find position, check for vertical/horizontal obstructions, move horizontally and check distance
-  def valid_move ? (destination_x, destination_y)
-    x_coord_distance = 0
-    x_coord_indices = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8 }
-    x_coord_distance = (x_coord_indices[destination_x] - x_coord_indices[x]).abs
+  def valid_move?(destination_x, destination_y)
+    return false if is_obstructed?(destination_x, destination_y) == true
 
+    # Return false if it is obstructed. - DONE
+    #
+    # Return true if a diagonal move
+    #  otherwise we return false
+    #
+    #
+    # How do we know if something is a diagonal move?
+    #  - Up 1 & Right 1 = true
+    #  - Up 2 & Right 2 = true
+    #  - Up 1 & Right 0 = false
+    #
+    #  1. How do we get the distance of the movement?
+
+    x_coord_indices = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8 }
+
+    x_coord_distance = (x_coord_indices[destination_x] - x_coord_indices[x]).abs
     y_coord_distance = (destination_y - y).abs
 
-    if vertical_move ? (destination_x)
-      !destination_piece.present ?
-    elsif horizontal_move ? (destination_y)
-      !destination_piece.present ?
-      else
-        x_coord_distance.abs <= 7 && y_coord_distance.abs <= 7
-        return true
-      end
-  end
+    #  2. Are the distances the same?
 
-  #starting position
-  def first_move?
-    if black?
-      y == 1 # starting at row 1
-    else
-      y == 8 # starting at row 8
-    end
   end
 end
