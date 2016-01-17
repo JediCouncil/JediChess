ab34f0a5ee9f0ec05ac28c3b325e07f3db5487ab
Yan Hu <iamyanhu@gmail.com>
2016-01-17 05:54:06 +0000

    converted x values to integers

--- app/models/pieces/queen.rb
+++ app/models/pieces/queen.rb
@@ -1,8 +1,8 @@
 class Queen < Piece
 	def valid_move?(destination_x, destination_y)
-		x_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-		x_array.map!.with_index{|x,i| i+1 } 
-		if (destination_x != x && destination_y == y) || (destination_y != y && destination_x == x) || (destination_x-x).abs == (destination_y-y).abs && !is_obstructed?(destination_x, destination_y)
+		x_coord_indices = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8 }
+
+		if (x_coord_indices[destination_x] != x_coord_indices[x] && destination_y == y) || (destination_y != y && x_coord_indices[destination_x] == x_coord_indices[x]) || (x_coord_indices[destination_x]-x_coord_indices[x]).abs == (destination_y-y).abs && !is_obstructed?(destination_x, destination_y)
 			true
 		else
 			false
