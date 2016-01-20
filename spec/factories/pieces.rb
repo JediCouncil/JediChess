41d35d6f346a3b3f3608f9b02c2e5820ced1de5d
eric orellana <eric.orellana92@gmail.com>
2016-01-20 20:58:43 +0000

    fixed merge conflicts

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -2,10 +2,13 @@ FactoryGirl.define do
   factory :piece do
     factory :pawn, class: 'Pawn' do
     end
-    factory :king, class: 'King' do
-    end
+
     factory :rook, class: 'Rook' do
     end
+
+    factory :king, class: 'King' do
+    end
+
     factory :knight, class: 'Knight' do
     end
   end
