1e577397bb5cafdecae1d61c182a4e027e1aa8e9
Yan Hu <iamyanhu@gmail.com>
2016-01-21 17:10:24 +0000

    Refactored obstruction to simpfly the logic. Now is also checking game_id to make sure only pieces in the same game could potentially pose as an obstruction

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe Queen, type: :model do
 
   describe "#valid_move?" do
-  	let(:white_queen) { build(:queen, color: "white", x: "D", y: 1) }
- 		let(:black_queen) { build(:queen, color: "black", x: "D", y: 8) }
-    let(:white_pawn)  { build(:pawn, color: "white", x: "D", y: 2)}  
+
+  	let(:white_queen) { build(:queen, color: "white", x: "D", y: 1, game_id: 9) }
+ 		let(:black_queen) { build(:queen, color: "black", x: "D", y: 8, game_id: 9) }
+    let(:white_pawn)  { build(:pawn, color: "white", x: "D", y: 2, game_id: 9)}
+    let(:white_pawn_2)  { build(:pawn, color: "white", x: "D", y: 2, game_id: 8)}    
 
  		it "white queen can move vertically" do
  			result = white_queen.valid_move?('D', 4)
@@ -17,6 +19,12 @@ RSpec.describe Queen, type: :model do
  			result = white_queen.valid_move?('D', 3)
  			expect(result).to be false
   	end
+
+    it "white queen - checking obstruction function must check the piece belonging to the same game" do
+      create(:pawn, x: 'D', y: 2, game_id: 8)
+      result = white_queen.valid_move?('D', 3)
+      expect(result).to be true
+    end
  
   	it "white queen can move diagonally" do
  			result = white_queen.valid_move?('F', 3)
@@ -39,22 +47,22 @@ RSpec.describe Queen, type: :model do
   	end  
 
   	# it "white queen cannot move to it's current position" do
- 		# 	result = white_queen.valid_move?('d', 1)
+ 		# 	result = white_queen.valid_move?('D', 1)
  		# 	expect(result).to be false
   	# end
 
   	# it "black queen cannot move to it's current position" do
- 		# 	result = black_queen.valid_move?('d', 8)
+ 		# 	result = black_queen.valid_move?('D', 8)
  		# 	expect(result).to be false
   	# end
 
   	# it "white queen cannot move to a non valid position" do
- 		# 	result = white_queen.valid_move?('c', 3)
+ 		# 	result = white_queen.valid_move?('C', 3)
  		# 	expect(result).to be false
   	# end
  
   	# it "black queen cannot move to a non valid position" do
- 		# 	result = black_queen.valid_move?('e', 6)
+ 		# 	result = black_queen.valid_move?('E', 6)
  		# 	expect(result).to be false
   	# end
 
