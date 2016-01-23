c8d02fb3ccc378ee6f6dca72d023df4b881ef2af
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-01-23 17:52:59 +0000

    added flash alert

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,6 +17,7 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     gon.pieces_hash = render_pieces(@game) # call the helper function and get the hash. will be passed onto the view
+    flash[:alert] = "The game is in check!" if @game.check?
   end
 
 end
