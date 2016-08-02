ff04f44776b1ab1960fe9107385927c7d64cd024
eric orellana <eric.orellana92@gmail.com>
2016-08-02 20:07:16 +0000

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module JediChess
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    #testing to precompile assets
+    #config.serve_static_assets = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
